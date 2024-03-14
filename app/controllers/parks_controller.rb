686af0ac0221f7a0c7960a850e91997899bdd03d
Robinpreet Kaur Malhi <rmalhi2@academic.rrc.ca>
2024-03-14 14:52:15 -0500

    added breadcrumb menu

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,10 +1,12 @@
 class ParksController < ApplicationController
   def index
     @parks = Park.all
+    
   end
 
   def show
     @park = Park.find(params[:id])
+    @tree = @park.trees.first
     @parks = Park.all
   end
 end
